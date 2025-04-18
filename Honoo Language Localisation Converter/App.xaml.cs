f7ea3c0b1bd3cad3a318e3959a0470a566e947cc
Loki Honoo <lokihonoo@hotmail.com>
2025-04-18 19:33:34 +0800

    #

--- Honoo Language Localisation Converter/App.xaml.cs
+++ Honoo Language Localisation Converter/App.xaml.cs
@@ -1,7 +1,4 @@
-﻿using Honoo.Configuration;
-using HonooLanguageLocalisationConverter.ViewModels;
-using System;
-using System.IO;
+﻿using HonooLanguageLocalisationConverter.ViewModels;
 using System.Windows;
 
 namespace HonooLanguageLocalisationConverter
@@ -11,65 +8,16 @@ namespace HonooLanguageLocalisationConverter
     /// </summary>
     public partial class App : Application
     {
-        private readonly string _configFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.xml");
-
         protected override void OnExit(ExitEventArgs e)
         {
-            using (var manager = new XConfigManager())
-            {
-                manager.Default.Properties.AddString("WindowState", Settings.Instance.WindowState.ToString());
-                manager.Default.Properties.AddString("WindowWidth", Settings.Instance.WindowWidth.ToString());
-                manager.Default.Properties.AddString("WindowHeight", Settings.Instance.WindowHeight.ToString());
-                manager.Default.Properties.AddString("LanguageFile", Settings.Instance.LanguageFile);
-                manager.Default.Properties.AddString("LastUpdate", Settings.Instance.LastUpdate.ToString("yyyy-MM-dd HH:mm:ss"));
-                manager.Save(_configFile);
-            }
+            Settings.Instance.Save();
             base.OnExit(e);
         }
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            string languageFile = string.Empty;
-            double width = 1200;
-            double height = 580;
-            WindowState state = WindowState.Normal;
-            try
-            {
-                using var manager = new XConfigManager(_configFile, true);
-                state = manager.Default.Properties.GetValue("WindowState", new XString(WindowState.Normal.ToString())).GetEnumValue<WindowState>();
-                width = manager.Default.Properties.GetValue("WindowWidth", new XString(1200.ToString())).GetDoubleValue();
-                height = manager.Default.Properties.GetValue("WindowHeight", new XString(580.ToString())).GetDoubleValue();
-                languageFile = manager.Default.Properties.GetStringValue("LanguageFile", string.Empty);
-                Settings.Instance.LastUpdate = manager.Default.Properties.GetValue("LastUpdate", new XString(DateTime.MinValue.ToString("yyyy-MM-dd"))).GetDateTimeValue();
-            }
-            catch
-            {
-            }
-            if (state == WindowState.Minimized)
-            {
-                state = WindowState.Normal;
-                width = 1200;
-                height = 580;
-            }
-            if (!string.IsNullOrEmpty(languageFile))
-            {
-                try
-                {
-                    LanguagePackage.Instance.Load(languageFile);
-                }
-                catch
-                {
-                    LanguagePackage.Instance.ResetDefault();
-                }
-            }
-            Size area = SystemParameters.WorkArea.Size;
-            Settings.Instance.WindowState = state;
-            Settings.Instance.WindowWidth = width;
-            Settings.Instance.WindowHeight = height;
-            Settings.Instance.WindowLeft = (area.Width - width) / 2;
-            Settings.Instance.WindowTop = (area.Height - height) / 2;
-            Settings.Instance.LanguageFile = languageFile;
+            Settings.Instance.Load();
         }
     }
 }
--- Honoo Language Localisation Converter/ViewModels/Settings.cs
+++ Honoo Language Localisation Converter/ViewModels/Settings.cs
@@ -1,5 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Honoo.Configuration;
+using HonooUI.WPF;
 using System;
+using System.ComponentModel;
+using System.IO;
 using System.Windows;
 
 namespace HonooLanguageLocalisationConverter.ViewModels
@@ -14,6 +18,8 @@ namespace HonooLanguageLocalisationConverter.ViewModels
 
         #region Members
 
+        private readonly string _configFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.xml");
+
         [ObservableProperty]
         private string? _languageFile;
 
@@ -24,7 +30,7 @@ namespace HonooLanguageLocalisationConverter.ViewModels
         private double _windowLeft;
 
         [ObservableProperty]
-        private WindowState _windowState;
+        private WindowState? _windowState;
 
         [ObservableProperty]
         private double _windowTop;
@@ -35,5 +41,68 @@ namespace HonooLanguageLocalisationConverter.ViewModels
         public DateTime LastUpdate { get; set; }
 
         #endregion Members
+
+        #region Construction
+
+        public Settings()
+        {
+        }
+
+        #endregion Construction
+
+        internal void Load()
+        {
+            string languageFile = string.Empty;
+            double width = 1200;
+            double height = 580;
+            WindowState state = System.Windows.WindowState.Normal;
+            try
+            {
+                using var manager = new XConfigManager(_configFile, true);
+                state = manager.Default.Properties.GetValue("WindowState", new XString(System.Windows.WindowState.Normal.ToString())).GetEnumValue<WindowState>();
+                width = manager.Default.Properties.GetValue("WindowWidth", new XString(1200.ToString())).GetDoubleValue();
+                height = manager.Default.Properties.GetValue("WindowHeight", new XString(580.ToString())).GetDoubleValue();
+                languageFile = manager.Default.Properties.GetStringValue("LanguageFile", string.Empty);
+                this.LastUpdate = manager.Default.Properties.GetValue("LastUpdate", new XString(DateTime.MinValue.ToString("yyyy-MM-dd"))).GetDateTimeValue();
+            }
+            catch
+            {
+            }
+            if (state == System.Windows.WindowState.Minimized)
+            {
+                state = System.Windows.WindowState.Normal;
+                width = 1200;
+                height = 580;
+            }
+            if (!string.IsNullOrEmpty(languageFile))
+            {
+                try
+                {
+                    LanguagePackage.Instance.Load(languageFile);
+                }
+                catch
+                {
+                    LanguagePackage.Instance.ResetDefault();
+                }
+            }
+            Size area = SystemParameters.WorkArea.Size;
+            this.WindowState = state;
+            this.WindowWidth = width;
+            this.WindowHeight = height;
+            this.WindowLeft = (area.Width - width) / 2;
+            this.WindowTop = (area.Height - height) / 2;
+            this.LanguageFile = languageFile;
+        }
+
+        internal void Save()
+        {
+            using var manager = new XConfigManager();
+            manager.Default.Properties.AddString("WindowState", this.WindowState.ToString());
+            manager.Default.Properties.AddString("WindowWidth", this.WindowWidth.ToString());
+            manager.Default.Properties.AddString("WindowHeight", this.WindowHeight.ToString());
+            manager.Default.Properties.AddString("LanguageFile", this.LanguageFile);
+            manager.Default.Properties.AddString("LastUpdate", this.LastUpdate.ToString("yyyy-MM-dd HH:mm:ss"));
+            manager.Save(_configFile);
+        }
     }
 }
